0eafdeedf3f48f31b9c8072fb03fdfb1042a4862
Zunayed <zunayed@gmail.com>
2014-01-15 17:38:25 -0500

    moving stuff around

--- 1.4_replace_whitespace.py
+++ 1.4_replace_whitespace.py
@@ -1,22 +1,28 @@
 # Write a method to replace all spaces in a string with '%20'.
-# Youmay assume that the string has sufficient space at the end 
-# of the string to hold the additional characters, and that you 
-# are given the "true" length of the string. 
+# You may assume that the string has sufficient space at the end
+# of the string to hold the additional characters, and that you
+# are given the "true" length of the string.
 
-#one way
+
 def replaceWhiteSpace(string):
-	new_string = ''
-	for item in string:
-		if item == ' ':
-			item = '%20'
+    temp = []
+    for item in string:
+        if item == ' ':
+            item = '%20'
+        #Avoid using the + and += operators to accumulate a string
+        #within a loop. Since strings are immutable, this creates
+        #unnecessary temporary objects and results in quadratic rather
+        #than linear running time. Instead, add each substring to a list
+        #and ''.join the list after the loop terminates
+        #(or, write each substring to a cStringIO.StringIO buffer).
+        temp.append(item)
 
-		new_string += item
+    return ''.join(temp)
 
-	return new_string
 
-#using list comprehension 
+#using list comprehension
 def replaceWhiteSpace2(string):
-	return ''.join(['%20' if item == ' ' else item for item in string])
+    return ''.join(['%20' if item == ' ' else item for item in string])
 
 
 print replaceWhiteSpace('What it do ?')
--- 1.5_string_compression.py
+++ 1.5_string_compression.py
@@ -1,18 +1,36 @@
-# Implement a method to perform basic string compression 
-# using the counts of repeated characters. For example, 
-# the string aabcccccaaa would become a2blc5a3. If the 
+# Implement a method to perform basic string compression
+# using the counts of repeated characters. For example,
+# the string aabcccccaaa would become a2blc5a3. If the
 # "compressed" string would not become smaller than the
 #  original string, your method should return the original string.
 
+
 def compressString(string):
-	new_string = ''
-	for item in string:
-		if item in new_string:
-			pass
-		else:
-			new_string += item + str(string.count(item))
+    new_string = ''
+    for char in string:
+        if char in new_string:
+            pass
+        else:
+            new_string += char + str(string.count(char))
 
-	return new_string
+    return new_string
 
-print compressString('aaaaabbbbccccccccc')
 
+def compressString2(string):
+    new_string = []
+    charCount = 0
+    lastChar = ''
+    for char in string:
+        if char == lastChar:
+            charCount += 1
+        else:
+            if lastChar != '':
+                new_string.append(lastChar + str(charCount))
+            charCount = 1
+        lastChar = char
+
+    new_string.append(lastChar + str(charCount))
+    return ''.join(new_string)
+
+print compressString('aaaaabbbbcccccccccd')
+print compressString2('aaaaabbbbcccccccccd')
