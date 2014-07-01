bcec6a001f8543631ab9f33509f8d18c6ca9fdf8
Zunayed Ali <zunayed@gmail.com>
2014-07-01 00:01:43 -0400

    added node deletion function

--- c++/Basic Data Structures/list.cpp
+++ c++/Basic Data Structures/list.cpp
@@ -6,22 +6,21 @@ using namespace std;
 
 class List{
 private:
-    typedef struct node{
+    struct node{
         int data;
         node* next_node;   
-    }* nodePtr;
+    };
 
-    nodePtr head;
-    nodePtr current;
-    nodePtr temp;
+    node* head;
+    node* current;
+    node* temp;
 
 public:
     List();
     void PrintList(); 
     void AddNode(int add_data);
-    // void DeleteNode(int del_data);
+    void DeleteNode(int del_data);
 };
-
 
 List::List(){
     head = NULL;
@@ -47,6 +46,37 @@ void List::AddNode(int data){
     }
 }
 
+
+void List::DeleteNode(int del_data){
+    temp = head;
+    current = head;
+    node* pointer_to_delete = NULL;
+
+    while(current != NULL && current->data != del_data){
+        temp = current;
+        current = current->next_node;
+    }
+
+    if(current == NULL){
+        cout << "element not  found";
+        delete pointer_to_delete;
+    }
+    else {
+        //found the data to delete
+        pointer_to_delete = current;
+        current = current->next_node;
+        temp->next_node = current;
+
+        if(pointer_to_delete == head){
+            head = head->next_node;
+            temp = NULL;
+        }
+
+        delete pointer_to_delete;
+        cout << del_data << " was deleted\n";
+    }
+}
+
 void List::PrintList(){
     current = head;
     while(current != NULL){
@@ -61,6 +91,11 @@ int main(int argc, char** argv){
     Test.AddNode(9);
     Test.AddNode(5);
     Test.AddNode(2);
+    Test.AddNode(1);
 
     Test.PrintList();
+
+    Test.DeleteNode(5);
+    Test.PrintList();
+
 }
