796a93556f967a0e9bd51b18e0e969a61c7ab605
Zunayed Ali <zunayed@gmail.com>
2014-07-28 20:54:14 -0400

    Stack in python

---
+++ Basic Data Structures/python/1_stacks.py
@@ -0,0 +1,31 @@
+class Stack:
+    """
+    LIFO, last-in first-out
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        """
+        Adds a new item to the top of the stack. It needs the item and returns nothing
+        """
+        self.items.append(item)
+
+    def pop(self):
+        """
+        Removes the top item from the stack. It needs no parameters and returns the item
+        """
+        return self.items.pop()
+
+    def peek(self):
+        """
+        Returns the top item from the stack but does not remove it. It needs no parameters
+        """
+        return self.items[-1]   # Alternately self.items[len(self.items) - 1]
+
+    def is_empty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
