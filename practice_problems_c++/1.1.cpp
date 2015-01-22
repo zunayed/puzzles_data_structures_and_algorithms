9b1989fb21d797b171fe9e03f9981ef361cebb66
Zunayed <zunayed@gmail.com>
2015-01-21 21:01:25 -0500

    inital solution

---
+++ practice_problems_c++/1.1.cpp
@@ -0,0 +1,31 @@
+// Does this string have unique char?
+
+#include <iostream>
+using namespace std;
+
+// ASCII maps to 128 characters
+const int CHAR_SET_SIZE = 128;
+
+bool string_has_unique_char(string input_string){
+	bool map[CHAR_SET_SIZE - 1]; 
+	cout << "Hello";	
+	for(int i = 0; i < input_string.length(); i++){
+		if (map[int(input_string[i])])
+			return false;
+		
+		map[int(input_string[i])] = true;
+	}
+	return true;
+}
+
+int main() {
+	string s1 = "abcdefg hij";
+	string s2 = "abcee329";
+	string s3 = "2921skj2";
+
+	cout << string_has_unique_char(s1) << endl;
+	cout << string_has_unique_char(s2) << endl;
+	cout << string_has_unique_char(s3) << endl;
+
+	return 0;
+}	
