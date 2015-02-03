67db6ecc8058db617eea648f4ad467eea6da0b31
zunayed <zunayed@gmail.com>
2015-02-02 21:13:08 -0500

    pointer solution

--- practice_problems_c++/2.1.cpp
+++ practice_problems_c++/2.1.cpp
@@ -1,16 +1,40 @@
 #include "data_structures/linked_list.h"
 
+void remove_dupes(List ll){
+    if (ll.head->next_node == NULL)
+        return;
+
+    List::node* current = ll.head;
+    while (current->next_node != NULL){
+        List::node* checker = current;
+        while (checker->next_node != NULL){
+            if (checker->next_node->data == current->data){
+                cout << "Found Duplicate\n";
+                cout << checker->next_node->next_node;
+                checker->next_node = checker->next_node->next_node;
+            } else {
+                checker = checker->next_node;
+            }
+        }
+        current = current->next_node;
+    }
+
+    ll.PrintList();
+}
 
 int main(){
     List Test;
 
     Test.AddNode(9);
-    Test.AddNode(5);
-    Test.AddNode(2);
     Test.AddNode(1);
-
+    Test.AddNode(4);
+    Test.AddNode(3);
+    Test.AddNode(3);
     Test.PrintList();
+    cout << "----Before remove function----" << endl;
+    remove_dupes(Test);
+    //cout << "----after remove function----" << endl;
+    //Test.PrintList();
 
-    Test.DeleteNode(5);
-    Test.PrintList();   
+
 }
