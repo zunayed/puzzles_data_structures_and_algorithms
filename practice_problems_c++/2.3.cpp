ebe699484f30fb432e737eee4671a46a7d8eebc3
Zunayed Ali <zunayed@gmail.com>
2015-02-11 23:38:33 -0500

    Update 2.3.cpp

--- practice_problems_c++/2.3.cpp
+++ practice_problems_c++/2.3.cpp
@@ -1,6 +1,5 @@
 /*
 Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node
-
 Input: the node c from the linked list a->b->c->d->e
 Result: nothing isreturned, but the new linked list looks like a- >b- >d->e
 */
