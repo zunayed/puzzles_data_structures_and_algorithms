4500a81474533e4964554d43560cfd3df4d79843
Zunayed Ali Morsalin <zunayed@gmail.com>
2016-01-03 12:52:33 -0500

    initial project skeleton

---
+++ bloom_filter/bloom_filter.py
@@ -0,0 +1,14 @@
+import hashlib
+
+import bitarray
+
+
+def hash(input_string):
+    """
+    in  -> byte stream
+    out -> 8 digit integer
+    """
+    return int(hashlib.sha1(input_string).hexdigest(), 16) % (10 ** 8)
+
+def main():
+    bit_array = (10 ** 5) * bit_array.bitarray('0')
