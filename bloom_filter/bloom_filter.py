23db7bfaaee3b38061e0dce96381da57cea6dcee
zunayed <zunayed@gmail.com>
2016-01-06 23:12:44 -0500

    calculating optimal number of hash functions

--- bloom_filter/bloom_filter.py
+++ bloom_filter/bloom_filter.py
@@ -1,21 +1,21 @@
-# TODO
-# load word lists
-# calculate optimal hash functions
-import sys
 import re
+import sys
+import math
 import hashlib
 
 import bitarray
 
 
-def fnv32a( str ):
+def fnv32a(input_string):
+    input_string = input_string.decode('utf-8')
     hval = 0x811c9dc5
     fnv_32_prime = 0x01000193
     uint32_max = 2 ** 32
-    for s in str:
+    for s in input_string:
         hval = hval ^ ord(s)
         hval = (hval * fnv_32_prime) % uint32_max
-    return hvalm
+
+    return hval
 
 
 def sha1(input_string):
@@ -23,7 +23,14 @@ def sha1(input_string):
     in  -> byte stream
     out -> 8 digit integer
     """
-    return int(hashlib.sha1(input_string).hexdigest(), 16) % (20)
+    return int(hashlib.sha1(input_string).hexdigest(), 16)
+
+
+def calc_optimal_hash_func(lenght_of_entries):
+    m = (-lenght_of_entries * math.log(0.01)) / ((math.log(2)) ** 2)
+    k = (m / lenght_of_entries) * math.log(2)
+
+    return k
 
 
 def load_words():
@@ -37,10 +44,19 @@ def load_words():
 
 
 def main():
-    bit_array = (20) * bitarray.bitarray('0')
-    words = load_words()
+    words       = load_words()
+    w_length    = len(words)
+    bit_array   = w_length * bitarray.bitarray('0')
 
-    import pdb;pdb.set_trace()
+    for word in words:
+        word = word.encode()
+        pos1 = fnv32a(word) % w_length
+        pos2 = sha1(word) % w_length
+        bit_array[pos1] = 1
+        bit_array[pos2] = 1
+
+    optimal = calc_optimal_hash_func(w_length)
+    import ipdb;ipdb.set_trace()
 
 if __name__ == '__main__':
     main()
