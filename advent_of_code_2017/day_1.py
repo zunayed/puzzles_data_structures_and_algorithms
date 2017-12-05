112a79c54311eeb827f8fd7d03898ab5b6315120
Zunayed Ali Morsalin <zunayed@gmail.com>
2017-12-04 23:56:21 -0500

    adding part 2 advent of code

--- advent_of_code_2017/day_1.py
+++ advent_of_code_2017/day_1.py
@@ -1,10 +1,16 @@
 #!/usr/local/bin/python3
 """
-Day 1
+--- Part One ---
 The captcha requires you to review a sequence of digits (your puzzle input)
 and find the sum of all digits that match the next digit in the list. The
 list is circular, so the digit after the last digit is the first digit in
 the list.
+
+--- Part Two ---
+Now, instead of considering the next digit, it wants you to consider the digit
+halfway around the circular list. That is, if your list contains 10 items,
+only include a digit in your sum if the digit 10/2 = 5 steps forward matches it.
+Fortunately, your list has an even number of elements.
 """
 
 def Input(day):
@@ -33,6 +39,10 @@ def part1(input):
     return count
 
 def part2(input):
+    """
+    input: string of digits
+    output: sum of repeating nums at the midway point
+    """
     count = 0
     N = len(input)
 
